f3417aa622b95636d24aff9f66bc1671f19ceb6e
Liliia <liliia.luzhanskaia@gmail.com>
2023-09-16 09:25:32 +0200

    feat: remove index.js :( SAT 16.09.2023 9:25 AM

--- cssMemeSlider/index.js
+++
@@ -1,8 +0,0 @@
-const IMAGES = document.querySelector(".images");
-const SLIDER = Array.from(document.querySelectorAll(".inputs"));
-
-SLIDER.forEach((switchElem, index) => {
-    switchElem.addEventListener('click', function() {
-        IMAGES.style.transform = `translateX(-${index * 475}px)`;
-      });
-});
